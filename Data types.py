86b9f0b5d2960265ea6db9018e8fc303042bbf96
Lamy18 <158460636+Lamy18@users.noreply.github.com>
2024-02-01 21:37:27 +1100

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,16 @@
+first_name = "Matt"
+last_name = "Lam"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+age = 16
+age_str = str(age)
+print(age_str)
+
+
+favorite_color = input("Enter your favorite color: ")
+if favorite_color == "red":
+    print("I love red too!")
+else:
+    print("That's okay, I like green better")
